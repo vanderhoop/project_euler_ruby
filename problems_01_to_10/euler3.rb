79f75a20bd6c4903a3c0fc2f932e9bd10223ada7
Travis Vander Hoop <vanderhoop@me.com>
2014-02-18 23:53:59 -0500

    switched out for loops for .each method in problem 3

--- problems_01_to_10/euler3.rb
+++ problems_01_to_10/euler3.rb
@@ -1,19 +1,20 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the
 # number 600851475143 ?
-
 num = 600851475143
 
 def is_prime?(num)
   return true if num == 2
   return false if num.even? || num <= 1
-  for i in (2..(num/2.round + 1))
+  (2..(num/2.round + 1)).each do |i|
     return false if num % i == 0
   end
   return true
 end
 
-for n in (3..num)
+high_factor = num
+
+(3..num).each do |n|
   if num % n == 0
     high_factor = num / n
     break if is_prime?(high_factor)
